739ce5095a0c71377c64bcdefdaa9bb2cc9c127f
Clint Ngo <clintngo@gmail.com>
2021-11-02 14:52:56 -0500

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+
+  const [poki,setPoki] = useState([])
+  useEffect(()=>{
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=807&offset=0')
+      .then(res=>{res.json()
+      .then(data=>{setPoki(data.results)
+      })
+    })
+    .catch(err=>{console.log(err)})
+  },[])
+  const [showPoki, setshowPoki] = useState(false)
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pokeapi</h1>
+        <button onClick={()=>setshowPoki(true)}>Fetch Pokemon</button>
       </header>
+      <br></br>
+      <ul>
+        {showPoki && poki.map((pokemon)=>{
+          return <li>{pokemon.name}</li>
+        })}
+      </ul>
     </div>
   );
 }
